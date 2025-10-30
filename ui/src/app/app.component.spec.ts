8f9b00c14a1d98c61727e08c6fce1aaf46198142
Guilherme Rodrigues <guilherme.rodrigues@ticketmaster.com>
2025-10-30 15:12:01 -0400

    fixing tests

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent,
+        HttpClientTestingModule,
+      ],
     }).compileComponents();
   });
 
@@ -24,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, game-deck');
+    expect(compiled.querySelector('h1')?.textContent).toContain('GO TO, Game deck');
   });
 });
